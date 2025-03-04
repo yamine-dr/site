a17da7d9a9b4575a3276ac04cdb846ee641d8027
Yamine Daroueche <yamine.9000@gmail.com>
2025-03-04 11:33:39 +0100

    added a counter

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,5 +1,20 @@
-export default function Header() {
+"use client";
+import React from "react";
+
+export default function Header(props) {
+    const [count, setCount] = React.useState(0);
+
     return (
-        <div>Hello World!</div>
+        <div>
+            <div className="text-2xl text-center">
+                {count}
+            </div>
+            <button
+                onClick={() => setCount(prevCount => prevCount + 1)}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                +
+            </button>
+        </div>
     );
 }
