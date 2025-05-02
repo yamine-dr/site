80307c8f8bff32728e4435b47a25ddaada2d2cb8
Yamine Daroueche <yamine.9000@gmail.com>
2025-05-02 23:53:48 +0200

    fix: "/portfolio/sportify" redirect

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/portfolio/sportify/:path*",
-                destination: "https://premium251.web-hosting.com/sportify/:path*",
+                destination: "portfolio.yaminedaroueche.com/sportify/:path*",
             },
         ];
     },
